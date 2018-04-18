8b36e62e5d94f72eba2ac6370ebcd5bd42f63c8f
Simon <guslofvisi@student.gu.se>
2018-04-18 15:35:56 +0200

    Added V2V-protocol

---
+++ v2v-protocol/build/CMakeFiles/V2V.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/simon/Desktop/Controller 17 April/v2v-protocol/build/Messages.cpp" "/home/simon/Desktop/Controller 17 April/v2v-protocol/build/CMakeFiles/V2V.dir/Messages.cpp.o"
+  "/home/simon/Desktop/Controller 17 April/v2v-protocol/V2VService.cpp" "/home/simon/Desktop/Controller 17 April/v2v-protocol/build/CMakeFiles/V2V.dir/V2VService.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
